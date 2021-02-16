ef083a3bbee5f598eaaefbd14021b16946f77206
Nbiba Bedis <bedisnbiba@gmail.com>
2021-02-16 14:13:30 +0100

    more colors + tab completion

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rustyline::{
     highlight::Highlighter,
     hint::Hinter,
     validate::{MatchingBracketValidator, ValidationContext, ValidationResult, Validator},
-    Cmd,
+    Cmd, Context,
 };
 use rustyline::{Editor, Helper};
 
@@ -17,6 +17,7 @@ mod utils;
 #[derive(Default)]
 struct IHsk {
     validator: MatchingBracketValidator,
+    hints: Vec<String>,
 }
 impl Helper for IHsk {}
 impl Validator for IHsk {
@@ -36,17 +37,50 @@ fn highlight_keywords(line: &str) -> String {
     line.replace(";", "\x1b[1;33m;\x1b[0m")
         .replace("let", "\x1b[1;32mlet\x1b[0m")
         .replace("where", "\x1b[1;31mwhere\x1b[0m")
+        .replace("data", "\x1b[1;34mdata\x1b[0m")
+        .replace("deriving", "\x1b[1;35mderiving\x1b[0m")
+        .replace("case", "\x1b[1;36mcase\x1b[0m")
+        .replace("of", "\x1b[1;36mof\x1b[0m")
+        .replace("if", "\x1b[1;33mif\x1b[0m")
+        .replace("else", "\x1b[1;33melse\x1b[0m")
+        .replace("then", "\x1b[1;33mthen\x1b[0m")
         .replace("=", "\x1b[1;33m=\x1b[0m")
         .replace("+", "\x1b[1;33m+\x1b[0m")
         .replace("-", "\x1b[1;33m-\x1b[0m")
         .replace("*", "\x1b[1;33m*\x1b[0m")
         .replace("/", "\x1b[1;33m/\x1b[0m")
+        .replace("(", "\x1b[1;33m(\x1b[0m")
+        .replace(")", "\x1b[1;33m)\x1b[0m")
+        .replace(">", "\x1b[1;33m>\x1b[0m")
+        .replace("<", "\x1b[1;33m<\x1b[0m")
+        .replace("^", "\x1b[1;33m^\x1b[0m")
+        .replace("|", "\x1b[1;33m|\x1b[0m")
 }
 impl Hinter for IHsk {
     type Hint = String;
 }
 impl Completer for IHsk {
     type Candidate = String;
+    fn complete(
+        &self,
+        line: &str,
+        pos: usize,
+        _ctx: &Context<'_>,
+    ) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
+        let last_word_start_pos = line[..pos].rfind(" ").map(|i| i + 1).unwrap_or(0);
+        let word_to_complete = &line[last_word_start_pos..pos];
+        if word_to_complete.is_empty() {
+            return Ok((0, vec![]));
+        }
+
+        for hint in self.hints.iter() {
+            if hint.starts_with(word_to_complete) {
+                let hint = hint.replace(word_to_complete, "");
+                return Ok((pos, vec![hint]));
+            }
+        }
+        return Ok((0, vec![]));
+    }
 }
 
 fn main() {
@@ -65,8 +99,16 @@ fn main() {
 
     loop {
         let readline = rl.readline("\x1b[1;33mIn: \x1b[0m");
+
         match readline {
             Ok(line) => {
+                rl.helper_mut().unwrap().hints.append(
+                    &mut line
+                        .split_whitespace()
+                        .map(|l| l.to_owned())
+                        .collect::<Vec<String>>(),
+                );
+
                 rl.add_history_entry(line.as_str());
                 tx_in.send(line.replace("\n", "") + "\n").unwrap();
                 let out = rx_out.recv().unwrap();
